dbba6cdd64c8fca8b3fbb2a4a7495cbb509c4b72
Wes Hediger <wes.hediger@gmail.com>
2016-06-04 13:10:47 -0600

    add random Picture gennerator

--- routes/index.js
+++ routes/index.js
@@ -22,24 +22,39 @@ var information = {
 
 console.log(information);
 
-
+// returns random pic file name by inserting a random num into name
+//format: download ('randomNumber').jpeg
+function getRandomPic() {
+    var randomPicNumber = Math.floor(Math.random() * 7);
+    var picFileName = '/../images/download-' + randomPicNumber +'.jpeg';
+    return picFileName;
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: information.title });
+  res.render('index', { title: information.title,
+      picture: getRandomPic()});
 });
 
 router.get('/how-to-shoot-a-bow', function(request, response){
   response.render('how-to-shoot-a-bow', { steps: information.firstSteps,
     title: 'First Steps',
-    back: '/',
+    back: '/how-to-shoot-a-bow-part-3',
     next: "/how-to-shoot-a-bow-part-2"})
-})
+});
 
 router.get('/how-to-shoot-a-bow-part-2', function(request, response) {
   response.render('how-to-shoot-a-bow-part-2', { steps: information.secondSteps,
     title: 'Second Steps',
     back: 'how-to-shoot-a-bow',
-    next: "how-to-shoot-a-bow-part-2"})
-})
+    next: "how-to-shoot-a-bow-part-3"})
+});
+
+router.get('/how-to-shoot-a-bow-part-3', function(request, response) {
+  response.render('how-to-shoot-a-bow-part-3', { steps: information.thirdSteps,
+    title: 'Third Steps',
+    back: 'how-to-shoot-a-bow-part-2',
+    next: "how-to-shoot-a-bow"})
+});
+
 module.exports = router;
